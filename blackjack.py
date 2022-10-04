f5c0fb3aa221fa687a3186494bba7a94b2fb0f63
GKurek94 <60116690+GKurek94@users.noreply.github.com>
2022-10-04 22:30:59 +0200

    Add files via upload

--- blackjack.py
+++ blackjack.py
@@ -41,11 +41,9 @@ def player_roll():
     pass
 
 
-def hands():
+def players_hand():
     player_hand = []
     player_hand_value = []
-    computer_hand = []
-    computer_hand_value = []
 
     while len(player_hand) != 2:
         var_p = random.choice(list(CARDS.keys()))
@@ -55,14 +53,22 @@ def hands():
         if len(player_hand) == 2:
             print(f"Player's cards are: {player_hand} and it's value is equal to: {sum(player_hand_value)}")
 
+        return sum(player_hand_value)
+
+
+def computers_hand():
+    computer_hand = []
+    computer_hand_value = []
+
     while len(computer_hand) != 2:
         var_c = random.choice(list(CARDS.keys()))
         val_var_c = CARDS[var_c]
         computer_hand.append(var_c)
         computer_hand_value.append(int(val_var_c))
         if len(computer_hand) == 2:
-            print(f"Computer's cards are X and  {player_hand[-1]}"
+            print(f"Computer's cards are X and  {computer_hand[-1]}"
                   f" and it's value is equal to: {sum(computer_hand_value)}")
+            return sum(computer_hand_value)
 
 
 def bet():
@@ -83,6 +89,14 @@ def main():
     balance = cash_in()
     while True:
         bets = bet()
+
+
+def game():
+    com_sum = computers_hand()
+    if com_sum == 21:
+        print("Computer has won.")
+    elif com_sum > 21:
+        print("Player won, because computer had value over 21. ")
 
 
 def game_of_player():
@@ -115,5 +129,5 @@ def game_of_player():
             print("2:", second_player)
 
 
-hands()
+print(computers_hand())
 
